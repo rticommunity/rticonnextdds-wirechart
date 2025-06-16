665990bb6d0ab52115c0bb80cd4dfa1a001a7540
Brian Reiche <breiche@rti.com>
2025-06-16 17:15:06 -0400

    Add function to get keys and topics.

--- src/flex_dictionary.py
+++ src/flex_dictionary.py
@@ -181,6 +181,28 @@ class FlexDict(dict):
         else:
             return list({k.domain for k in self.keys() if k.topic == topic})
 
+    def get_all_topics_and_domains(self) -> tuple:
+        """
+        Retrieves all unique topics and domains from the keys in the FlexDict.
+
+        This method iterates through all the keys in the dictionary and extracts
+        the unique topics and domains.
+
+        Returns:
+            tuple: A tuple containing two sets:
+                - The first set contains all unique topics.
+                - The second set contains all unique domains.
+
+        Example:
+            If the FlexDict contains the following keys:
+                Key('topic1', 1), Key('topic2', 2), Key('topic1', 3)
+            The method will return:
+                ({'topic1', 'topic2'}, {1, 2, 3})
+        """
+        domains = {key.domain for key in self.keys()}
+        topics = {key.topic for key in self.keys()}
+        return topics, domains
+    
     def most_nodes(self, top_n=6, topic=None, domain=None):
         """
         Returns the top N keys with the most set elements, optionally filtered by topic or domain.
@@ -282,3 +304,6 @@ if __name__ == "__main__":
 
     print("\nToDict:")
     print(d.to_dict())
+
+    print("\nPrint All Keys")
+    print(d.get_all_topics_and_domains())
